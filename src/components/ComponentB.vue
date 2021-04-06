f12ad89398c037584ae6d1076e1507d4b71dee7a
gohomewho <ga676005@hotmail.com.tw>
2021-04-06 21:52:14 +0800

    Stop listening to event before object is destroyed

--- src/components/ComponentB.vue
+++ src/components/ComponentB.vue
@@ -13,16 +13,19 @@ export default {
   components: {
     ComponentC,
   },
-
-  data: function () {
+  created() {
     this.$bus.$on("say-hello", () => {
       //$on使用EventBus的監聽事件
       this.message = "Hello from Component A";
     });
-
+  },
+  data: function () {
     return {
       message: "Lonely Component Here!",
     };
+  },
+  beforeDestroy: function () {
+    this.$bus.$off("say-hello");
   },
 };
 </script>
