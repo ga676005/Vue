58d8f179d7d78f2502ee9131e02ab57d98a3f05d
gohomewho <ga676005@hotmail.com.tw>
2021-04-07 15:29:06 +0800

    Count total price of items in the shipping cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,11 +2,11 @@
   <div>
     <div>
       <ul>
-        <li class="price-row">
-          <div>Old Red Friend</div>
+        <li class="price-row" v-for="product in products" :key="product.id">
+          <div>{{ product.name }}</div>
           <div class="quantity-row">
-            <div class="price-quantity">Qty: 2</div>
-            <div>$29.97</div>
+            <div class="price-quantity">Qty: {{ product.qty }}</div>
+            <div>{{ formatPrice(product.price * product.qty) }}</div>
           </div>
         </li>
       </ul>
@@ -14,15 +14,15 @@
 
     <div class="price-row">
       <div class="price-label">Sub-total</div>
-      <div class="price-wrapper">$9.99</div>
+      <div class="price-wrapper">{{ formatPrice(subtotal) }}</div>
     </div>
     <div class="price-row">
       <div class="price-label">Shipping</div>
-      <div class="price-wrapper">$9.99</div>
+      <div class="price-wrapper">{{ formatPrice(shipping) }}</div>
     </div>
     <div class="price-row">
       <div class="price-label">Total</div>
-      <div class="price-wrapper">$9.99</div>
+      <div class="price-wrapper">{{ formatPrice(total) }}</div>
     </div>
     <button class="checkout-button">CHECKOUT</button>
   </div>
@@ -36,10 +36,26 @@ export default {
       this.addProduct(product);
     });
   },
+
   data: function () {
     return {
       products: [],
     };
+  },
+
+  computed: {
+    subtotal() {
+      return this.products.reduce(
+        (sum, item) => sum + item.price * item.qty,
+        0
+      );
+    },
+    shipping() {
+      return this.products.reduce((sum, item) => sum + item.qty * 399, 0);
+    },
+    total() {
+      return this.subtotal + this.shipping;
+    },
   },
 
   methods: {
@@ -55,6 +71,9 @@ export default {
         ...product,
         qty: 1,
       });
+    },
+    formatPrice(cents) {
+      return "$" + cents / 100;
     },
   },
 
