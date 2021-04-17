7176bc853b66027f12ea38ef6683d1939c45dcd8
gohomewho <ga676005@hotmail.com.tw>
2021-04-17 09:32:36 +0800

    initial

---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }} (mounted)</p>
+  <p class="rank">{{ rank }} (computed)</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) return (this.rank = 'Nunja Fingers')
+    if (this.score < 400) return (this.rank = 'Rapid Reflexex')
+    return (this.rank = 'Snail pace...')
+  },
+  computed: {
+    computedRank() {
+      if (this.score < 250) return 'Nunja Fingers'
+      if (this.score < 400) return 'Rapid Reflexex'
+      return 'Snail pace...'
+    }
+  }
+}
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
